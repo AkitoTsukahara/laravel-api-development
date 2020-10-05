3bd0cc5a820055d8aa1960880980e14bf57d38d8
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-10-05 22:49:37 +0900

    [add]Homeページ vue適用

--- server/app/Http/Controllers/AppController.php
+++ server/app/Http/Controllers/AppController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class AppController extends Controller
 {
     /**
      * Create a new controller instance.
