2561e9ef9ec905b3fc0b0f9a9a33f16d98ecbbb8
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-10-12 22:20:28 +0900

    [add]validation test

--- server/tests/Feature/ContactsTest.php
+++ server/tests/Feature/ContactsTest.php
@@ -12,10 +12,54 @@ class ContactsTest extends TestCase
 
     use RefreshDatabase;
 
-    public function test_a_contact_can_be_added()
+    /**
+     * @test
+     */
+    public function a_contact_can_be_added()
     {
         $this->withoutExceptionHandling();
-        $this->post('/api/contacts',['name' => 'Test Name']);
+        $this->post('/api/contacts',[
+            'name' => 'Test Name',
+            'email' => 'test@email.com',
+            'birthday' => '2000/01/01',
+            'company' => 'ABC String',
+        ]);
+
+        $contact = Contact::first();
+
         $this->assertCount(1,Contact::all());
+        $this->assertEquals('Test Name',$contact->name);
+        $this->assertEquals('test@email.com',$contact->email);
+        $this->assertEquals('2000/01/01',$contact->birthday);
+        $this->assertEquals('ABC String',$contact->company);
+    }
+
+    /**
+     * @test
+     */
+    public function a_name_is_required(){
+
+        $response = $this->post('/api/contacts',[
+            'email' => 'test@email.com',
+            'birthday' => '2000/01/01',
+            'company' => 'ABC String',
+        ]);
+
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0,Contact::all());
+    }
+
+    /**
+     * @test
+     */
+    public function email_is_required(){
+        $response = $this->post('/api/contacts',[
+            'name' => 'Test Name',
+            'birthday' => '2000/01/01',
+            'company' => 'ABC String',
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0,Contact::all());
     }
 }
