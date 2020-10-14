4f91f6930d7ae9f618834a11cee1a7fa02482f6e
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-10-15 00:11:01 +0900

    [add]validation update

--- server/tests/Feature/ContactsTest.php
+++ server/tests/Feature/ContactsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Contact;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -22,10 +23,10 @@ class ContactsTest extends TestCase
 
         $contact = Contact::first();
 
-        $this->assertCount(1,Contact::all());
+        //$this->assertCount(1,Contact::all());
         $this->assertEquals('Test Name',$contact->name);
         $this->assertEquals('test@email.com',$contact->email);
-        $this->assertEquals('2000/01/01',$contact->birthday);
+        $this->assertEquals('2000-01-01',$contact->birthday->format('Y-m-d'));
         $this->assertEquals('ABC String',$contact->company);
     }
 
@@ -44,6 +45,37 @@ class ContactsTest extends TestCase
                 $this->assertCount(0,Contact::all());
             });
     }
+
+    /**
+     * @test
+     * email形式のテスト
+     */
+    public function email_must_be_a_valid_email(){
+        $response = $this->post('/api/contacts',
+            array_merge($this->data(),['email' => 'NOT AN EMAIL'])
+        );
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0,Contact::all());
+    }
+
+    /**
+     * @test
+     * 日付形式のテスト
+     */
+    public function birthdays_are_properly_stored(){
+
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/api/contacts',
+            array_merge($this->data())
+        );
+
+        $this->assertCount(1,Contact::all());
+        $this->assertInstanceOf(Carbon::class,Contact::first()->birthday);
+        $this->assertEquals('2000-01-01',Contact::first()->birthday->format('Y-m-d'));
+    }
+
 
     /**
      * @test
@@ -75,7 +107,7 @@ class ContactsTest extends TestCase
         return [
             'name' => 'Test Name',
             'email' => 'test@email.com',
-            'birthday' => '2000/01/01',
+            'birthday' => '2000-01-01',
             'company' => 'ABC String'
         ];
     }
