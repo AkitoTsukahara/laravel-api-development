8b9b3a1aaba6145630fa82ea3e2142b32a20a358
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-10-31 14:33:53 +0900

    [add]Edit Contacts

--- server/resources/js/views/ContactsShow.vue
+++ server/resources/js/views/ContactsShow.vue
@@ -4,10 +4,12 @@
         <div v-else>
             <div class="flex justify-between">
                 <div class="text-blue-400">
-                    < Back
+                    <a href="#" class="text-blue-400" @click="$router.back()">
+                        < Back
+                    </a>
                 </div>
                 <div class="relative">
-                    <router-link :to="'/contact/' + contact.contact_id + '/edit'" class="px-4 py-2 rounded text-green-500 border border-green-500 text-sm font-bold mr-2">Edit</router-link>
+                    <router-link :to="'/contacts/' + contact.contact_id + '/edit'" class="px-4 py-2 rounded text-green-500 border border-green-500 text-sm font-bold mr-2">Edit</router-link>
                     <a href="#" class="px-4 py-2 border border-red-500 rounded text-sm font-bold text-red-500" @click="modal = ! modal">Delete</a>
                     <div v-if="modal" class="absolute bg-blue-900 text-white rounded-lg z-20 p-8 w-64 right-0 mt-2 mr-6">
                         <p>Are you sure you want to delete this record?</p>
